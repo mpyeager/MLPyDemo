b1ffd17c405ec853a42e57474792668c6e24ac52
Matthew Yeager <mpyeager@icloud.com>
2017-01-23 14:08:18 +0000

    Update twitter_sentiment_analysis_demo.py

--- twitter_sentiment_analysis_demo.py
+++ twitter_sentiment_analysis_demo.py
@@ -1,11 +1,15 @@
 import tweepy
 from textblob import TextBlob
 
-consumer_key = "ybYvSQdCc4OFnsEX0WZZR3DHd"
-consumer_secret = "UPw2NXtTZnoAPS07lKxjY1LI5VakLv1jEM1SnJG0C3ejy5UQnl"
+#Ensure that you have a Twitter account linked to a mobile phone number, then
+#generate your keys and secrets below from the Application Management section of
+#the Twitter developer portal.
 
-access_token = "18505899-uKMOQixny6KHC5gdqsBZdhabgpT6seq043Ph1TlCe"
-access_token_secret = "4PdowtvG50jZsbsOCWN8s4Z8MBoIMzcG7SeykPZmXalrx"
+consumer_key = "INSERT_YOUR_CONSUMER_KEY"
+consumer_secret = "INSERT_YOUR_CONSUMER_KEY_SECRET"
+
+access_token = "INSERT_YOUR_ACCESS_TOKEN"
+access_token_secret = "INSERT_YOUR_ACCESS_TOKEN_SECRET"
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
